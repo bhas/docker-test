275acfd38af2149065ce2657cd4511715c956ed0
BJORN-VINTHER <97097933+BJORN-VINTHER@users.noreply.github.com>
2024-04-25 22:02:03 +0200

    Add sample endpoint

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # docker-test
+
+
+## Getting started
+1. Install and run docker.
+2. Run the project. if you get a `Connection is not private` error you may need to restart your browser. 
---
+++ src/Api/Dtos/OrderListDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Api.Dtos;
+
+public class OrderListDto
+{
+    public required string OrderNumber { get; set; }
+    public required string CustomerName { get; set; }
+    public DateTimeOffset DateTimeOffset { get; set; }
+    public int TotalAssets => Assets.Count;
+    public required List<OrderListAssetDto> Assets { get; set; } = new List<OrderListAssetDto>();
+
+    public OrderListDto() { } // empty constructor is added to avoid potential deserialization issues
+
+}
+
+public class OrderListAssetDto()
+{
+    public required string AssetId { get; set; }
+    public int Quantity { get; set; }
+}
