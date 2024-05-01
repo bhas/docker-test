34683f9032988403aa4f0dc179a1bd12061d6a10
BJORN-VINTHER <97097933+BJORN-VINTHER@users.noreply.github.com>
2024-05-01 22:26:56 +0200

    add migration

--- src/Infrastructure/Database/Migrations/20240501202458_AddDistributionTable.Designer.cs
+++ src/Infrastructure/Database/Migrations/20240501202458_AddDistributionTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Database.Migrations
 {
     [DbContext(typeof(DockerTestContext))]
-    partial class DockerTestContextModelSnapshot : ModelSnapshot
+    [Migration("20240501202458_AddDistributionTable")]
+    partial class AddDistributionTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Database/Migrations/20240501202458_AddDistributionTable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDistributionTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Distributions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    DistributionConfigId = table.Column<long>(type: "bigint", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    StatusText = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Distributions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DistributionAssetLink",
+                columns: table => new
+                {
+                    AssetId = table.Column<string>(type: "text", nullable: false),
+                    DistributionId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DistributionAssetLink", x => new { x.DistributionId, x.AssetId });
+                    table.ForeignKey(
+                        name: "FK_DistributionAssetLink_Distributions_DistributionId",
+                        column: x => x.DistributionId,
+                        principalTable: "Distributions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DistributionAssetLink");
+
+            migrationBuilder.DropTable(
+                name: "Distributions");
+        }
+    }
+}
